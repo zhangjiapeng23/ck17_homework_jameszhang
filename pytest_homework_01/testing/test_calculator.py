9846f52f2f0135da9b8cb8463b9b9ed1cae33e2c
james.zhang <2665353662@qq.com>
2021-01-29 17:02:02 +0800

    1. add div function testcases. 2. add div test data.

--- pytest_homework_01/testing/test_calculator.py
+++ pytest_homework_01/testing/test_calculator.py
@@ -10,6 +10,8 @@ import allure
 from python_project.Calculator import Calculator
 
 
+@allure.feature("Get two numbers sum.")
+@pytest.mark.add
 class TestCalculatorAdd:
 
     def setup_class(self):
@@ -20,7 +22,6 @@ class TestCalculatorAdd:
 
     def setup(self):
         print("【开始计算】")
-
 
     def teardown(self):
         print("【计算结束】")
@@ -37,23 +38,96 @@ class TestCalculatorAdd:
 
         return data['add'][genre]['ids']
 
-    @pytest.mark.add
+    @allure.story("Get two int numbers sum.")
     @pytest.mark.parametrize(('a', 'b', 'res'), data_yaml('int'), ids=ids_yaml('int'))
     def test_add_int(self, a, b, res):
         with allure.step('step 1: sum the two numbers.'):
-            sum_ = self.cal.add(a, b)
+            res_ = self.cal.add(a, b)
 
         with allure.step('step 2: check the result'):
-            assert sum_ == res
+            assert res_ == res
 
-    @pytest.mark.add
+    @allure.story("Get two float numbers sum.")
     @pytest.mark.parametrize(('a', 'b', 'res'), data_yaml('float'), ids=ids_yaml('float'))
     def test_add_float(self, a, b, res):
         with allure.step('step 1: sum the two numbers.'):
-            sum_ = self.cal.add(a, b)
+            res_ = self.cal.add(a, b)
 
         with allure.step('step 2: check the result'):
-            assert round(sum_, 2) == round(res, 2)
+            assert round(res_, 3) == round(res, 3)
+
+
+@allure.feature("divide two numbers.")
+@pytest.mark.div
+class TestCalculatorDiv:
+
+    def setup_class(self):
+        self.cal = Calculator()
+
+    def teardown_class(self):
+        del self.cal
+
+    def setup(self):
+        print("【开始计算】")
+
+    def teardown(self):
+        print("【计算结束】")
+
+    def data_yaml(genre):
+        with open('data/calculator_test_data.yml') as f:
+            data = yaml.safe_load(f)
+
+        return data['div'][genre]['data']
+
+    def ids_yaml(genre):
+        with open('data/calculator_test_data.yml') as f:
+            data = yaml.safe_load(f)
+
+        return data['div'][genre]['ids']
+
+    @allure.story("divide two int numbers.")
+    @pytest.mark.parametrize(('a', 'b', 'res'), data_yaml('int'), ids=ids_yaml('int'))
+    def test_div_int(self, a, b, res):
+        with allure.step('step 1: divide two numbers.'):
+            res_ = self.cal.div(a, b)
+
+        with allure.step('step 2: check the result'):
+            assert round(res_, 2) == round(res, 2)
+
+    @allure.story("divide two float numbers.")
+    @pytest.mark.parametrize(('a', 'b', 'res'), data_yaml('float'), ids=ids_yaml('float'))
+    def test_div_float(self, a, b, res):
+        with allure.step('step 1: divide two numbers.'):
+            res_ = self.cal.div(a, b)
+
+        with allure.step('step 2: check the result'):
+            assert round(res_, 2) == round(res, 2)
+
+    @allure.story("when divisor is zero")
+    @allure.severity(allure.severity_level.CRITICAL)
+    @pytest.mark.parametrize(('a', 'b', 'res'), data_yaml('divisor_zero'), ids=ids_yaml('divisor_zero'))
+    def test_div_zero_divisor(self, a, b, res):
+        with allure.step('step 1: divide two numbers.'):
+            try:
+                res_ = self.cal.div(a, b)
+            except ZeroDivisionError:
+                res_ = 'error'
+
+        with allure.step('step 2: check the result'):
+            assert res_ == res
+
+
+@pytest.mark.mul
+class TestCalculatorMul:
+    # todo: add mul function testcases
+    pass
+
+
+@pytest.mark.sub
+class TestCalculatorSub:
+    # todo: add sub function testcases
+    pass
+
 
 
 
